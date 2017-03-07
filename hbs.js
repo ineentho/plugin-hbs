de0d7a712677f35d31307425c458ce1f544809a5
Henrik Karlsson <henrik@taxicaller.com>
2017-03-07 12:15:22 +0100

    Remove normalizeSync because it causes problems with SystemJS 0.20

--- hbs.js
+++ hbs.js
@@ -2,8 +2,6 @@
 
 import Handlebars from 'handlebars';
 
-const handlebarsRuntimePath = System.normalizeSync('handlebars/handlebars.runtime', __moduleName);
-
 export function translate(load) {
   var precompiled = Handlebars.precompile(load.source);
   load.source = `module.exports = require('handlebars/handlebars.runtime').template(${precompiled});`;
